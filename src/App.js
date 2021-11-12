ee161b7cd351d16c1bd3acaf6cec72dcbe8b442a
Emanuel Bessa <41740085+Emanuelbessa@users.noreply.github.com>
2021-11-12 02:47:25 -0300

    feat: :lipstick: update frontpage, add Dashboard component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from "./components/Dashboard";
+import useStyles from "./styles/AppStyles";
 
 function App() {
+  const classes = useStyles();
+  const {App} = classes;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={App}>
+      <Dashboard />
     </div>
   );
 }
